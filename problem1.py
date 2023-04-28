aeaad9dc254043d175389414d95f95544dfc864c
Family <albon.136546120855@2depedqc.ph>
2023-04-28 19:14:38 +0800

    Loop has been constructed

--- problem1.py
+++ problem1.py
@@ -15,6 +15,11 @@ with open("numbers.txt", "r") as problem_one_numbers_file:
 even_data = []
 odd_data = []
 # construct the loop that will identify between even and odd numbers
+for number in numbers_data:
+    if int(number) % 2 == 0:
+        even_data.append(number)
+    else:
+        odd_data.append(number)
 # open the file and transfer the classified even numbers to designated text file named "even.txt"
 # open the file and transfer the classified even numbers to designated text file named "odd.txt"
 # check the newly created text files and if the problem is solved.
