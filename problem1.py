d75cea8672d6473f7649bc22f4b8ed42ce494210
Family <albon.136546120855@2depedqc.ph>
2023-04-28 18:59:42 +0800

    Instruction

--- problem1.py
+++ problem1.py
@@ -2,3 +2,6 @@ print("Problem 01 ~ Assignment 4".center(42, "="))
 print("\033[94m=" * 42)
 print("\033[91mCMPE-103-MODULE-2-FILE-HANDLING-IN-PYTHON")
 print("\033[94m=" * 42)
+
+# write a python program that reads a text file, then the program will classify the even and odd numbers
+# into separate text files extracted from the main text file.
